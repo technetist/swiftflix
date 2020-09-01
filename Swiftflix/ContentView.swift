2482ed0c2d55c13763f691cb0767e038c269a5b0
Adrien Maranville <Adrien@Maranville.io>
2020-09-01 12:08:20 -0600

    Initial Commit

---
+++ Swiftflix/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Swiftflix
+//
+//  Created by Adrien Maranville on 9/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftflix/SwiftflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftflixApp.swift
+//  Swiftflix
+//
+//  Created by Adrien Maranville on 9/1/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
